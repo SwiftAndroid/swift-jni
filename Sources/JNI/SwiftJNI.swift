d4bb403dce8aabf5eb8b7c7a26b458b80b1c72fb
rikner <rikner@web.de>
2017-08-04 13:57:04 +0200

    add GetByteArrayRegion and SetByteArrayRegion

--- Sources/JNI/SwiftJNI.swift
+++ Sources/JNI/SwiftJNI.swift
@@ -199,6 +199,25 @@ extension JNI {
         return result
     }
 
+    public func GetByteArrayRegion(array: JavaByteArray, startIndex: Int = 0, numElements: Int = -1) -> [UInt8] {
+        let _env = self._env
+        var count = numElements
+
+        if numElements < 0 {
+            count = GetLength(array)
+        }
+
+        var result = [JavaByte](repeating: 0, count: count)
+        _env.pointee.pointee.GetByteArrayRegion(_env, array, jsize(startIndex), jsize(count), &result)
+        return result.map { UInt8($0) }
+    }
+
+    public func SetByteArrayRegion(array: JavaByteArray, startIndex: Int = 0, from sourceElements: [Int]) {
+        let _env = self._env
+        var newElements = sourceElements.map { JavaByte($0) } // make mutable copy
+        _env.pointee.pointee.SetArrayRegion(_env, array, jsize(startIndex), jsize(newElements.count), &newElements)
+    }
+
     public func GetIntArrayRegion(array: JavaIntArray, startIndex: Int = 0, numElements: Int = -1) -> [Int] {
         let _env = self._env
         var count = numElements
