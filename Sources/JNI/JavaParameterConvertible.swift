fb1cc567f8c2b4cb9a90c1345ca5916fe7dbfeca
Geordie J <geojay@gmail.com>
2017-12-21 14:27:06 +0100

    Add JavaContext struct

--- Sources/JNI/JavaParameterConvertible.swift
+++ Sources/JNI/JavaParameterConvertible.swift
@@ -147,3 +147,38 @@ extension JavaObject: JavaParameterConvertible {
         return try jni.GetObjectField(of: javaObject, id: fieldID)
     }
 }
+
+
+public struct JavaContext: JavaParameterConvertible {
+    private let object: JavaObject
+    public init(_ object: JavaObject) {
+        self.object = object
+    }
+
+    private static let javaClassname = "android/content/Context"
+    public static let asJNIParameterString = "L\(javaClassname);"
+
+    public func toJavaParameter() -> JavaParameter {
+        return JavaParameter(object: self.object)
+    }
+
+    public static func fromStaticField(_ fieldID: JavaFieldID, of javaClass: JavaClass) throws -> JavaContext {
+        let jobject: JavaObject = try jni.GetStaticObjectField(of: javaClass, id: fieldID)
+        return self.init(jobject)
+    }
+
+    public static func fromMethod(calling methodID: JavaMethodID, on object: JavaObject, args: [JavaParameter]) throws -> JavaContext {
+        let jObject = try jni.CallObjectMethod(methodID, on: object, parameters: args)
+        return self.init(jObject)
+    }
+
+    public static func fromStaticMethod(calling methodID: JavaMethodID, on javaClass: JavaClass, args: [JavaParameter]) throws -> JavaContext {
+        let jObject = try jni.CallStaticObjectMethod(methodID, on: javaClass, parameters: args)
+        return self.init(jObject)
+    }
+
+    public static func fromField(_ fieldID: JavaFieldID, on javaObject: JavaObject) throws -> JavaContext {
+        let javaStringObject = try jni.GetObjectField(of: javaObject, id: fieldID)
+        return self.init(javaStringObject)
+    }
+}
