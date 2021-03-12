ffa9bf7e1df9ec83c4a23613f7a7e8cb8dc48155
rikner <rikner@web.de>
2021-03-12 16:55:53 +0100

    implement JNIObject.getField()

--- Sources/JNI/JNIFields.swift
+++ Sources/JNI/JNIFields.swift
@@ -25,6 +25,14 @@ public extension JNI {
         try checkAndThrowOnJNIError()
         return try T.fromField(fieldID!, on: javaObject)
     }
+
+    func GetField(_ fieldName: String, fieldJavaClassName: String, from javaObject: JavaObject) throws -> JavaObject {
+        let env = self._env
+        let javaClass = try GetObjectClass(obj: javaObject)
+        let fieldID = env.pointee.pointee.GetFieldID(env, javaClass, fieldName, "L\(fieldJavaClassName);")
+        try checkAndThrowOnJNIError()
+        return try JavaObject.fromField(fieldID!, on: javaObject)
+    }
 }
 
 
