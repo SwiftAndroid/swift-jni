894237c1e1cb874c5b1e07a28c649be0cfb9e548
Geordie Jay <geojay@gmail.com>
2022-11-23 19:06:28 +0100

    Fix isMainThread for Darwin

--- Sources/JNI/isMainThread.swift
+++ Sources/JNI/isMainThread.swift
@@ -1,12 +1,17 @@
 #if canImport(Glibc)
 import Glibc
-#elseif canImport(Darwin)
-import Darwin
-#endif
 
 @_silgen_name("syscall")
 public func syscallNonVariadic(_ number: Int) -> Int
 
 public var isMainThread: Bool {
-    syscallNonVariadic(Int(SYS_gettid)) == getpid()
+    return syscallNonVariadic(Int(SYS_gettid)) == getpid()
 }
+#elseif canImport(Darwin)
+import Darwin
+import Foundation // free on Darwin, expensive elsewhere
+
+public var isMainThread: Bool {
+    return Thread.isMainThread
+}
+#endif
